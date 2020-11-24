f9e532726a7b273e54e2353afef07a70989f6d1c
Clara <>
2020-11-24 17:51:21 +0100

    routing home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookComponent } from './book/book.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { ReserveComponent } from './reserve/reserve.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent, children: [
+    { path: '', pathMatch: 'full', redirectTo: '/home/map' },
+    { path: 'map', component: MapComponent },
+    { path: 'book', component: BookComponent },
+    { path: 'reservation', component: ReserveComponent }
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { LibroReservasComponent } from './libro-reservas/libro-reservas.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { MapComponent } from './map/map.component';
+import { BookComponent } from './book/book.component';
+import { ReserveComponent } from './reserve/reserve.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LibroReservasComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    MapComponent,
+    BookComponent,
+    ReserveComponent
   ],
   imports: [
     BrowserModule,
