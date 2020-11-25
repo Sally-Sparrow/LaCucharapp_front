02b52ae4cefb4e499649224ca1a0b2e95238fb6d
DESKTOP-06IB626\alfre <alfredofweb@gmail.com>
2020-11-25 17:14:41 +0100

    Formulario reservas funcionando, faltan validaciones

--- src/app/reserve/reserve.component.ts
+++ src/app/reserve/reserve.component.ts
@@ -13,11 +13,32 @@ export class ReserveComponent implements OnInit {
 
   constructor() {
     this.form = new FormGroup({
+      nombre: new FormControl,
+      fecha: new FormControl,
+      personas: new FormControl,
+      turno_comida: new FormControl,
+      turno_cena: new FormControl,
+      numero_mesa1: new FormControl,
+      numero_mesa2: new FormControl,
+      numero_mesa3: new FormControl,
+      numero_mesa4: new FormControl,
+      numero_mesa5: new FormControl,
+      numero_mesa6: new FormControl,
+      numero_mesa7: new FormControl,
+      numero_mesa8: new FormControl,
+      numero_mesa9: new FormControl,
+      numero_mesa10: new FormControl,
+      nota: new FormControl,
 
     })
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+
   }
 
 }
